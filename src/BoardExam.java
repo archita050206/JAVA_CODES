a7d22256101ec440f73bab97a223fa887d4c3e85
Archita Das <darchita06@gmail.com>
2024-12-23 13:44:42 +0530

    board exam percentage calculation

---
+++ src/BoardExam.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class BoardExam {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Physics: ");
+        float phy=sc.nextFloat();
+        sc.nextLine();
+        System.out.println("Chemistry: ");
+        float chem=sc.nextFloat();
+        sc.nextLine();
+        System.out.println("Biology: ");
+        float bio=sc.nextFloat();
+        sc.nextLine();
+        System.out.println("Mathematics: ");
+        float maths=sc.nextFloat();
+        sc.nextLine();
+        float sum=(phy+chem+bio+maths)/4.0f;
+        System.out.println("Percentage is: "+sum+"%");
+    }
+}
