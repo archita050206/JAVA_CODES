95f0e702ecc8458daa28e4ac97eb97a0aaa826e1
Archita Das <darchita06@gmail.com>
2024-12-23 13:06:12 +0530

    java codes

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int a=5;
+        int b=10;
+        int t=a+b;
+        
+        System.out.println("Sum = "+t);
+    }
+}
