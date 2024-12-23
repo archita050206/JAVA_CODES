991035d60179eaa6d142487472700a90d0c5e611
Archita Das <darchita06@gmail.com>
2024-12-23 13:54:22 +0530

    print greeting message after taking name

---
+++ src/greet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class greet {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String str=sc.nextLine();
+        System.out.println("Hello "+str+"! Have a good day!");
+    }
+    
+}
