f0334664fb0db15766cb107c4112ec17409d08f4
Archita Das <darchita06@gmail.com>
2024-12-23 20:29:30 +0530

    pattern printing 1

---
+++ src/pattern1.java
@@ -0,0 +1,10 @@
+public class pattern1 {
+    public static void main(String [] args){
+        for(int i=0;i<4;i++){
+            for(int j=(4-i);j>=1;j--){
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
