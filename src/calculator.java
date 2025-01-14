ed03b0a512c11db0414dc1b163fde54b9ef2a7d4
Archita Das <darchita06@gmail.com>
2025-01-14 20:01:11 +0530

    basic  printing in java

---
+++ src/calculator.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Operations{
+    public int sum(int a,int b){
+        return a+b;
+    }
+    public int subtract(int a, int b){
+        return a-b;
+    }
+    public int multi(int a, int b){
+        return a*b;
+    }
+    public int divide(int a,int b){
+        return a/b;
+    }
+};
+public class calculator {
+    public static void main(String args[]){
+        Boolean b=true;
+        while(b){
+            
+            System.out.println("Enter the two numbers: ");
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the operation: ");
+            String str=sc.nextLine();
+            int n1=sc.nextInt();
+            int n2=sc.nextInt();
+            
+
+        }
+    }
+}
---
+++ src/class1.java
@@ -0,0 +1,12 @@
+public class class1 {
+    public static void main(String args[]){
+        String msg1="my name is Mohit";
+        int v6=100;
+        double v7=3.14;
+        boolean flag=true;;
+        System.out.println(msg1);
+        System.out.println(v6);
+        System.out.println(v7);
+        System.out.println(flag);
+    }
+}
