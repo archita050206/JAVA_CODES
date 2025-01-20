1aba72ccc75bf97350c779e409079e10f41fa9fa
Archita Das <darchita06@gmail.com>
2025-01-20 20:59:55 +0530

    finding missing number in array

---
+++ src/MissingNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MissingNumber {
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int range=sc.nextInt();
+    int arr[]=new int[range];
+    for(int i=0;i<range;i++){
+        int x=sc.nextInt();
+        arr[i]=x;
+    }
+    boolean b=false;
+    for(int j=0;j<range;j++){
+        if(arr[j]!=j+1){
+            b=true;
+            System.out.println(j+1);
+            break;
+        }
+    }
+    if(!b)System.out.println("No missing element");
+}
+}
