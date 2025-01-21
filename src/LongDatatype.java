bcbc66195f5b5539b14911097c4447d8b09ed40f
Archita Das <darchita06@gmail.com>
2025-01-21 19:18:46 +0530

    form of representation of long numbers

---
+++ src/LongDatatype.java
@@ -0,0 +1,7 @@
+public class LongDatatype {
+    public static void main(String args[]){
+        long x=123_456_789_078l;//between underscore there can be any number of digits
+        long y=123456789078l;
+        System.out.print(x+"\n"+y);
+    }
+}
