e92e8331c88f5b75975024fdf449d38408dfc186
Archita Das <darchita06@gmail.com>
2025-01-21 19:27:26 +0530

    number systems : bin, oct, hex

---
+++ src/NumberSystems.java
@@ -0,0 +1,9 @@
+public class NumberSystems {
+    public static void main(String args[]){
+        int x=10;
+        int bin=0b1010;
+        int hex=0xA;
+        int oct=012;
+        System.out.println(x+" "+bin+" "+hex+" "+oct);
+    }
+}
